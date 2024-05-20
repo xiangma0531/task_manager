30a3ba831ceb231a567b4dc8dd811705181d4c5f
xiangma0531 <shoma0531@outlook.com>
2024-05-20 22:37:35 +0900

    デフォルトロケール日本語設定

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module TaskManager
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # デフォルトロケールを日本語に設定
+    config.i18n.default_locale = :ja
   end
 end
