764d407d5a686ff245335eec097288ad09a8fd87
xiangma0531 <shoma0531@outlook.com>
2024-05-20 22:04:14 +0900

    アソシエーション・バリデーション設定

--- app/models/task_detail.rb
+++ app/models/task_detail.rb
@@ -1,3 +1,7 @@
 class TaskDetail < ApplicationRecord
   belongs_to :task_header
+
+  validates :name, presence: true
+  validates :due_date, presence: true
+  validates :completed, inclusion: { in: [true, false] }
 end
--- app/models/task_header.rb
+++ app/models/task_header.rb
@@ -1,3 +1,6 @@
 class TaskHeader < ApplicationRecord
   belongs_to :user
+  has_many :task_details, dependent: :destroy
+
+  validates :title, presence: true
 end
