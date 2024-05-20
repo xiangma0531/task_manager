5db94cb56e29529a526078f627b350fad4d5dae7
xiangma0531 <shoma0531@outlook.com>
2024-05-20 22:21:09 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'task_headers#index'
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :task_headers do
+    resources :task_details
+  end
+
 end
